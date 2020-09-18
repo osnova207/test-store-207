1312cc4cdbc761af7edfa2540cf75d6172457e4d
Osnova207 <osnova207@gmail.com>
2020-09-18 21:32:58 +0300

    added firebase initializing

--- src/index.js
+++ src/index.js
@@ -4,9 +4,23 @@ import App from './App';
 import {Provider} from 'react-redux';
 import {createStore} from "redux";
 import reducer from "./reducers/index";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../src/assets/styles.scss';
+import * as firebase from 'firebase';
 
 const store = createStore(reducer);
+
+const config = {
+    apiKey: "AIzaSyBpDHJHL3GUpHNCOlGmSjGYtI1N-cAuS7c",
+    authDomain: "test-store-207.firebaseapp.com",
+    databaseURL: "https://test-store-207.firebaseio.com",
+    projectId: "test-store-207",
+    storageBucket: "test-store-207.appspot.com",
+    messagingSenderId: "317363076755",
+    appId: "1:317363076755:web:c211eca407fcd959bfd766"
+};
+
+firebase.initializeApp(config);
 
 ReactDom.render(
     <Provider store={store}>
