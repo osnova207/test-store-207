cf3354aaf22301f7b073613ad00229238f68ed8b
Osnova207 <osnova207@gmail.com>
2020-09-25 15:28:28 +0300

    added zoom img for product card

--- src/components/Product-card.js
+++ src/components/Product-card.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import {connect} from "react-redux";
 import {Link, withRouter} from "react-router-dom";
+import * as cn from "classnames";
+import OutsideClickHandler from "react-outside-click-handler";
 
 const ProductCard = (props) => {
 
     const { id } = props.match.params;
     const product = props.products.find((product) => product.id === Number(id)) || {};
     const { name, price, description, image, date, customProperties } = product;
+
+    const [imgZoom, setImgZoom] = useState(false);
 
     const renderCustomProperties = () => (
         customProperties?.map((property) => (
@@ -27,7 +31,11 @@ const ProductCard = (props) => {
         <div className='ProductCard'>
             <Link to='/products-list/' className='ProductCard__back-link button'>Вернуться</Link>
             <div className="ProductCard__title-block">
-                <div className='ProductCard__img' style={{backgroundImage: `url(${image})`}} />
+                <div className="ProductCard__img-container" onClick={() => setImgZoom(true)}>
+                    <OutsideClickHandler onOutsideClick={() => setImgZoom(false)}>
+                        <img className={cn('ProductCard__img', {'img-zoom': imgZoom})} src={image} alt='product-img' />
+                    </OutsideClickHandler>
+                </div>
                 <div className='ProductCard__title'>
                     <h1>{name}</h1>
                     <p>{description}</p>
