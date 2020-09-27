930cd247e15feea6fa56b1d53cd779a6bd653899
Osnova207 <osnova207@gmail.com>
2020-09-27 21:13:55 +0300

    change values per page on Pagination

--- src/components/views/Pagination.js
+++ src/components/views/Pagination.js
@@ -8,54 +8,54 @@ const Pagination = ({ pageCount, perPage, currentPage, onChangePage, onChangePer
     const list = pageCountArr.map(item => (
         <div
             key={item}
-            className={cn("pagination__item", {'pagination__item-active': item === currentPage})}
+            className={cn("Pagination__item", {'Pagination__item-active': item === currentPage})}
             onClick={() => onChangePage(item)}>
             {item}
         </div>
     ));
 
     return (
-        <div className="pagination">
+        <div className="Pagination">
             {pageCount > 1 &&
-                <React.Fragment>
+                <div className="Pagination__buttons">
                     {pageCount > 2 &&
-                    <button className="pagination__arrow"
+                    <button className="Pagination__arrow"
                             onClick={() => onChangePage(1)}
                             disabled={currentPage === 1}>
                         <span className="material-icons">first_page</span>
                     </button>}
-                    <button className="pagination__arrow"
+                    <button className="Pagination__arrow"
                             onClick={() => onChangePage(currentPage - 1)}
                             disabled={currentPage === 1}>
                         <span className="material-icons">chevron_left</span>
                     </button>
                     {list}
-                    <button className="pagination__arrow"
+                    <button className="Pagination__arrow"
                             onClick={() => onChangePage(currentPage + 1)}
                             disabled={currentPage === pageCount}>
                         <span className="material-icons">chevron_right</span>
                     </button>
                     {pageCount > 2 &&
-                    <button className="pagination__arrow"
+                    <button className="Pagination__arrow"
                             onClick={() => onChangePage(pageCount)}
                             disabled={currentPage === pageCount}>
                         <span className="material-icons">last_page</span>
                     </button>}
-                </React.Fragment>}
+                </div>}
 
-            <div className="pagination__per-page">
-                <div className="pagination__page-count-text">
+            <div className="Pagination__per-page">
+                <div className="Pagination__page-count-text">
                     На странице:
                 </div>
-                <div className={cn("pagination__page-count", {"pagination__page-count-active": perPage === 10})}
-                     onClick={() => onChangePerPage(10)}>
-                    10
+                <div className={cn("Pagination__page-count", {"Pagination__page-count-active": perPage === 15})}
+                     onClick={() => onChangePerPage(15)}>
+                    15
                 </div>
-                <div className={cn("pagination__page-count", {"pagination__page-count-active": perPage === 20})}
-                     onClick={() => onChangePerPage(20)}>
-                    20
+                <div className={cn("Pagination__page-count", {"Pagination__page-count-active": perPage === 30})}
+                     onClick={() => onChangePerPage(30)}>
+                    30
                 </div>
-                <div className={cn("pagination__page-count", {"pagination__page-count-active": perPage === 50})}
+                <div className={cn("Pagination__page-count", {"Pagination__page-count-active": perPage === 50})}
                      onClick={() => onChangePerPage(50)}>
                     50
                 </div>
